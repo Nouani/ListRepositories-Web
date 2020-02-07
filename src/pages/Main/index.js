e88de8f81ce014191bbbb9acb71e6e2fc606b85a
Nouani <nouanigabriel@hotmail.com>
2020-02-07 18:05:41 -0300

    Listando repositorios e navegando

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
     state = {
@@ -11,6 +12,22 @@ export default class Main extends Component {
         repositories: [],
         loading: false,
     };
+
+    componentDidMount() {
+        const repositories = localStorage.getItem('repositories');
+
+        if (repositories) {
+            this.setState({ repositories: JSON.parse(repositories) });
+        }
+    }
+
+    componentDidUpdate(_, prevState) {
+        const { repositories } = this.state;
+
+        if (prevState.repositories !== repositories) {
+            localStorage.setItem('repositories', JSON.stringify(repositories));
+        }
+    }
 
     handleInputChange = e => {
         this.setState({ newRepo: e.target.value });
@@ -36,7 +53,7 @@ export default class Main extends Component {
     };
 
     render() {
-        const { newRepo, loading } = this.state;
+        const { newRepo, loading, repositories } = this.state;
 
         return (
             <Container>
@@ -60,6 +77,20 @@ export default class Main extends Component {
                         )}
                     </SubmitButton>
                 </Form>
+                <List>
+                    {repositories.map(repository => (
+                        <li key={repository.name}>
+                            <span>{repository.name}</span>
+                            <Link
+                                to={`/repository/${encodeURIComponent(
+                                    repository.name
+                                )}`}
+                            >
+                                Detalhes
+                            </Link>
+                        </li>
+                    ))}
+                </List>
             </Container>
         );
     }
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository() {
-    return <h1>Repository</h1>;
+export default class Repository extends Component {
+    async componentDidMount() {
+        const { match } = this.props;
+
+        const repoName = decodeURIComponent(match.params.repository);
+
+        const reponse = await api.get(`/repos/${repoName}`);
+        const issues = await api.get(`/repos/${repoName}/issues`);
+    }
+
+    render() {
+        return <h1>Repository</h1>;
+    }
 }
